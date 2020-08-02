8b3738bb56ebd8593fd3416d6ff18363e9ea2ed0
Przemyslaw Konieczniak <przemyslaw.konieczniak@gmail.com>
2020-08-02 05:00:10 +0200

    21-remove-page-limit

--- src/components/active-page-select/ActivePageSelect.js
+++ src/components/active-page-select/ActivePageSelect.js
@@ -2,11 +2,9 @@ import { Element } from '../common';
 import { searchCriteriaChangedEvent } from '../../events';
 
 export const ActivePageSelect = (pages, currentPage) => {
-    const MAX_PAGES = 100; // TODO: Use a generator fn, to handle huge numbers of pages
-    const condtion = pages > MAX_PAGES ? MAX_PAGES : pages;
     const children = [];
 
-    for (let page = 1; page <= condtion; page++) {
+    for (let page = 1; page <= pages; page++) {
         children.push(
             Element('option', {
                 options: {
