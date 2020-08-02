9a3c41dfe617a6dcefebb951a2ece24f03f1b57e
Przemyslaw Konieczniak <przemyslaw.konieczniak@gmail.com>
2020-08-02 04:28:59 +0200

    14-clean-up-main.js

--- src/main.js
+++ src/main.js
@@ -15,43 +15,51 @@ import {
     ReadLaterList,
 } from './components';
 
+(() => {
+    const onSearchCriteriaChanged = async (event) => {
+        const result = await getNews(event.detail);
+        document.querySelector('.newsList').replaceWith(NewsList(result.results));
+        document
+            .querySelector('#activePageSelect')
+            .replaceWith(ActivePageSelect(result.pages, result.currentPage));
+    };
+
+    const onSaveArticle = (event) => {
+        saveArticle(event.detail);
+        const bookmarkedNews = getBookmarkedNews();
+        document
+            .querySelector('.readLaterList')
+            .replaceWith(ReadLaterList(bookmarkedNews));
+    };
+
+    const onDeleteArticle = (event) => {
+        deleteArticle(event.detail);
+        const bookmarkedNews = getBookmarkedNews();
+        document
+            .querySelector('.readLaterList')
+            .replaceWith(ReadLaterList(bookmarkedNews));
+    };
+
+    document.body.addEventListener(
+        'searchCriteriaChanged',
+        onSearchCriteriaChanged
+    );
+    document.body.addEventListener('saveArticle', onSaveArticle);
+    document.body.addEventListener('deleteArticle', onDeleteArticle);
+
+    document.querySelector('#sectionSelectColumn').appendChild(SectionSelect());
+    document.querySelector('#searchColumn').appendChild(SearchInput());
+})();
+
 window.onload = async () => {
-    const result = await getNews();
-    document.querySelector('#newsListColumn').append(NewsList(result.results));
+    const { results, pages, currentPage } = await getNews();
+    document.querySelector('#newsListColumn').append(NewsList(results));
     document
         .querySelector('#activePageSelectColumn')
-        .appendChild(ActivePageSelect(result.pages, result.currentPage));
+        .appendChild(ActivePageSelect(pages, currentPage));
 
     const bookmarkedNews = getBookmarkedNews();
     document
         .querySelector('#readLaterListColumn')
         .appendChild(ReadLaterList(bookmarkedNews));
 };
-
-const body = document.body;
-body.addEventListener('searchCriteriaChanged', async (event) => {
-    const result = await getNews(event.detail);
-    document.querySelector('.newsList').replaceWith(NewsList(result.results));
-    document
-        .querySelector('#activePageSelect')
-        .replaceWith(ActivePageSelect(result.pages, result.currentPage));
-});
-
-body.addEventListener('saveArticle', async (event) => {
-    saveArticle(event.detail);
-    const bookmarkedNews = getBookmarkedNews();
-    document
-        .querySelector('.readLaterList')
-        .replaceWith(ReadLaterList(bookmarkedNews));
-});
-
-body.addEventListener('deleteArticle', async (event) => {
-    deleteArticle(event.detail);
-    const bookmarkedNews = getBookmarkedNews();
-    document
-        .querySelector('.readLaterList')
-        .replaceWith(ReadLaterList(bookmarkedNews));
-});
-
-document.querySelector('#sectionSelectColumn').appendChild(SectionSelect());
-document.querySelector('#searchColumn').appendChild(SearchInput());
